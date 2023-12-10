134721873c769a2f611ebfc3e570bcafbbc8ac6a
Heinrich van Wyk <heinrichvw3410@gmail.com>
2023-12-10 13:59:40 +0200

    moved model

---
+++ Models/TaxBracket.cs
@@ -0,0 +1,14 @@
+﻿using SARSTaxBracketScraper;
+using SARSTaxBracketScraper.Models;
+
+namespace SARSTaxBracketScraper.Models
+{
+    public class TaxBracket
+    {
+        public DateTime? Start { get; set; }
+        public DateTime? End { get; set; }
+        public List<Bracket> Brackets { get; set; }
+    }
+}
+
+
