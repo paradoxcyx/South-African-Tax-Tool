e14b7279bab4c0df7d6812af59755187c67a6e89
Heinrich van Wyk <heinrichvw3410@gmail.com>
2023-12-10 14:34:52 +0200

    Added API project

---
+++ southafricantaxtool.API/Controllers/TaxController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace southafricantaxtool.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TaxController : ControllerBase
+    {
+        private readonly ILogger<TaxController> _logger;
+
+        public TaxController(ILogger<TaxController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            _logger.LogDebug("Hit endpoint");
+            await Task.Delay(500);
+            return Ok("success");
+        }
+    }
+}
